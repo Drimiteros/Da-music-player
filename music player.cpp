cb3a5ecb88810ba917eb3b6e000e825e4d85e94e
Drimiteros <dimthan2@gmail.com>
2025-03-19 04:30:39 +0200

    Add files via upload

--- music player.cpp
+++ music player.cpp
@@ -3,15 +3,25 @@
 #include <SFML/Audio.hpp>
 #include <filesystem>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 using namespace sf;
 
 int main()
 {
-    string version = "Da music player v.1.1_2";
-    RenderWindow window(VideoMode(1000, 750), version);
+    string version = "Da music player v.1.2";
+    RenderWindow window(VideoMode(995, 747), version, Style::Close);
     Event event;
+
+    ifstream loadFile("src/Saves/save.txt");
+    string this_line;
+    string load_file_path;
+    while (getline(loadFile, this_line)) {
+        if (this_line.find("latest directory: ") == 0) {
+            load_file_path = this_line.substr(18);
+        }
+    }
 
     Texture play_button_texture;
     Texture next_button_texture;
@@ -33,13 +43,10 @@ int main()
     next_button.setPosition(125, 600);
 
     Font font;
-    if (!font.loadFromFile("src/Fonts/font.ttf")) {
-        cout << "Error loading font" << endl;
-        return -1;
-    }
+    font.loadFromFile("src/Fonts/font.ttf");
 
     vector<Text> directory_text_vector;
-    vector<string> file_paths; 
+    vector<string> file_paths;
     Text directory_text;
     directory_text.setFont(font);
     directory_text.setCharacterSize(15);
@@ -58,15 +65,24 @@ int main()
 
     Clock clock;
     Clock clock2;
-    string file_path;
+    string file_path = load_file_path;
     vector<string>file_name;
     vector<float>file_size;
-    bool stop_search = true;
+    bool stop_search = false;
 
     SoundBuffer soundBuffer;
     Sound sound;
     float sound_offset = 0;
     float sound_duration = 0;
+    VertexArray waveform(LineStrip, 770);
+    const Int16* samples = nullptr;
+    size_t sample_count = 0;
+    size_t channel_count = 0;
+    RectangleShape waveformArea;
+    waveformArea.setSize(Vector2f(770, 142));
+    waveformArea.setPosition(220, 600);
+    waveformArea.setFillColor(Color(30, 30, 30));
+    float scaleY = 0.003;
 
     RectangleShape view;
     view.setSize(Vector2f(1000, 500));
@@ -74,7 +90,7 @@ int main()
     float height = 0;
     RectangleShape select_bar;
     select_bar.setSize(Vector2f(1000, 20));
-    select_bar.setFillColor(Color(70, 70, 255));
+    select_bar.setFillColor(Color(255, 155, 55));
     select_bar.setPosition(99999, 99999);
     RectangleShape cursor;
     cursor.setSize(Vector2f(1, 1));
@@ -84,16 +100,15 @@ int main()
     line[1].setSize(Vector2f(1000, 1));
     line[1].setPosition(0, 580);
     line[2].setSize(Vector2f(2, 20));
-    line[2].setPosition(0, 5);
+    line[2].setPosition(file_path_text.getLocalBounds().getSize().x + 10, 5);
     line[2].setFillColor(Color(255, 220, 155));
     RectangleShape timestamp_bar;
     timestamp_bar.setSize(Vector2f(1000, 20));
     timestamp_bar.setPosition(10, 725);
-    timestamp_bar.setFillColor(Color(70, 70, 255));
+    timestamp_bar.setFillColor(Color(255, 155, 55));
     RectangleShape timestamp_bar_max;
     timestamp_bar_max.setPosition(5, 720);
     timestamp_bar_max.setFillColor(Color(255, 255, 255));
-
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
@@ -134,7 +149,7 @@ int main()
                 && sound.getStatus() == sound.Playing)
                 sound.pause();
             else if (((event.type == Event::KeyPressed && Keyboard::isKeyPressed(Keyboard::Space))
-                || (cursor.getGlobalBounds().intersects(play_button.getGlobalBounds()) && event.type == Event::MouseButtonPressed && Mouse::isButtonPressed(Mouse::Left))) 
+                || (cursor.getGlobalBounds().intersects(play_button.getGlobalBounds()) && event.type == Event::MouseButtonPressed && Mouse::isButtonPressed(Mouse::Left)))
                 && sound.getStatus() == sound.Paused)
                 sound.play();
         }
@@ -172,13 +187,13 @@ int main()
             directory_text_vector.clear();
             try {
                 for (const auto& entry : filesystem::directory_iterator(file_path)) {
-                    string full_path = entry.path().string(); 
+                    string full_path = entry.path().string();
                     file_name.push_back(entry.path().filename().string());
                     file_size.push_back(entry.file_size());
                     file_paths.push_back(full_path);
 
                     Text temp_text = directory_text;
-                    temp_text.setString(entry.path().filename().string()); 
+                    temp_text.setString(entry.path().filename().string());
                     directory_text_vector.push_back(temp_text);
                 }
                 stop_search = true;
@@ -189,6 +204,30 @@ int main()
             }
         }
 
+        if (soundBuffer.getSampleCount() > 0 && sound.getStatus() != Sound::Stopped) {
+            samples = soundBuffer.getSamples();
+            sample_count = soundBuffer.getSampleCount();
+            channel_count = soundBuffer.getChannelCount();
+            size_t startSample = sound.getPlayingOffset().asSeconds() * soundBuffer.getSampleRate() * channel_count;
+            for (size_t i = 0; i < 770; ++i) {
+                size_t sampleIndex = startSample + (i * 5);
+                if (sampleIndex < sample_count) {
+                    float sampleValue = 0;
+                    for (size_t channel = 0; channel < channel_count; ++channel) {
+                        if (sampleIndex + channel < sample_count)
+                            sampleValue += samples[sampleIndex + channel];
+                    }
+                    sampleValue /= channel_count;
+                    waveform[i].position = Vector2f(waveformArea.getPosition().x + i, waveformArea.getPosition().y + (waveformArea.getSize().y / 2) + (sampleValue * scaleY));
+                    float intensity = abs(sampleValue) / 32768.0f;
+                    waveform[i].color = Color(150 + 190 * intensity, 100 + 195 * intensity, 55);
+                }
+                else {
+                    waveform[i].position = Vector2f(waveformArea.getPosition().x + i, waveformArea.getPosition().y + (waveformArea.getSize().y / 2));
+                    waveform[i].color = Color(50, 50, 250);
+                }
+            }
+        }
         window.clear();
         window.draw(line[0]);
         window.draw(line[1]);
@@ -200,7 +239,6 @@ int main()
         window.draw(file_info_text);
         window.draw(timestamp_bar_max);
         window.draw(timestamp_bar);
-
         for (int i = 0; i < directory_text_vector.size(); i++) {
             directory_text_vector[i].setPosition(5, 45 + height + (i * 20));
             if (cursor.getGlobalBounds().intersects(directory_text_vector[i].getGlobalBounds()) && directory_text_vector[i].getGlobalBounds().intersects(view.getGlobalBounds())) {
@@ -211,7 +249,7 @@ int main()
                     if (soundBuffer.loadFromFile(selected_file)) {
                         sound.setBuffer(soundBuffer);
                         sound_duration = soundBuffer.getDuration().asSeconds();
-                        file_info_text.setString("Now playing: " + file_name[i] + "\nFile size: " + to_string(file_size[i] / 1048576).substr(0, 5) + " Mb" + "\nDuration: " + to_string(sound_duration).substr(0, 5) + " sec");
+                        file_info_text.setString(file_name[i].substr(0, 21) + "...\nFile size: " + to_string(file_size[i] / 1048576).substr(0, 5) + " Mb" + "\nDuration: " + to_string(sound_duration).substr(0, 5) + " sec");
                         sound.play();
                     }
                 }
@@ -224,9 +262,14 @@ int main()
             if (view.getGlobalBounds().intersects(directory_text_vector[i].getGlobalBounds()))
                 window.draw(directory_text_vector[i]);
         }
-
+        window.draw(waveformArea);
+        window.draw(waveform);
         window.display();
     }
+
+    ofstream saveFile("src/Saves/save.txt");
+    saveFile.clear();
+    saveFile << "latest directory: " + file_path;
 
     return 0;
 }
